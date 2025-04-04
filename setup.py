c591c3a47ddf4c995db44a5c2ca7c7fd8438f2fa
Jhai-pranesh <jhaipranesh@gmail.com>
2025-04-04 10:54:07 +0530

    Folder structure addded

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "GenerativeAI Project",
+    version = "0.0.0",
+    author = "Jhai Pranesh",
+    packages = find_packages(),
+    install_requires = []
+)
